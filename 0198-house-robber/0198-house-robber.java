43230a968bdad697c4b36b9e88f18b5304bbe1dc
Himanshu Yadav <42291590+1himanshu1804442@users.noreply.github.com>
2026-07-11 14:04:20 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -7,16 +7,17 @@ class Solution {
         return solve(nums,n-1);
     }
     public int solve(int nums[],int ind){
+        if(ind<0)return 0;
         if(ind==0){
             return nums[0];
 
         }
         if(dp[ind]!=-1)return dp[ind];
         int notpick=solve(nums,ind-1);
-        int pick=nums[ind];
-        if(ind>=2){
-         pick=solve(nums,ind-2)+nums[ind];
-}
+       
+        
+         int pick=solve(nums,ind-2)+nums[ind];
+
 return dp[ind]=Math.max(pick,notpick);
 
     }
