be112416d1203f7e7afb1626304ba04409a8adf9
Himanshu Yadav <42291590+1himanshu1804442@users.noreply.github.com>
2026-08-08 19:32:26 +0530

    Time: 8 ms (24.67%), Space: 47.1 MB (49.55%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int indegree[]=new int[numCourses];
+        List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for(int it[]:prerequisites){
-            int u=it[0];
-            int v=it[1];
-            adj.get(u).add(v);
+        for(int pre[]:prerequisites){
+            int u=pre[0];
+            int v=pre[1];
+            adj.get(v).add(u);
         }
-        int indegree[]=new int[numCourses];
         for(int i=0;i<numCourses;i++){
             for(int it:adj.get(i)){
                 indegree[it]++;
@@ -17,26 +17,25 @@ class Solution {
         }
         Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-           if(indegree[i]==0)q.add(i);
+            if(indegree[i]==0){
+                q.offer(i);
+            }
         }
-        List<Integer> list=new ArrayList<>();
+        int completed=0;
         while(!q.isEmpty()){
-            int node=q.poll();
-            list.add(node);
-            for(int it:adj.get(node)){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.add(it);
+            int curr=q.poll();
+            completed++;
+            for(int neighbour:adj.get(curr)){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.offer(neighbour);
                 }
+
             }
 
+
         }
-        if(list.size()==numCourses){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return completed==numCourses;
         
     }
 }
