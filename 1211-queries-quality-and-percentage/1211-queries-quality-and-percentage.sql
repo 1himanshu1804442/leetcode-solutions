f408fe90ea03482d107f1a9dec4c38f17f2a467e
Himanshu Yadav <42291590+1himanshu1804442@users.noreply.github.com>
2026-08-14 13:54:18 +0530

    Time: 488 ms (12.89%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -3,5 +3,5 @@ SELECT query_name,
 ROUND(AVG(rating/position),2) AS quality,
 ROUND(AVG(IF(rating<3,100,0)),2) AS poor_query_percentage
 FROM Queries
-WHERE query_name IS NOT null
+
 GROUP BY query_name;
